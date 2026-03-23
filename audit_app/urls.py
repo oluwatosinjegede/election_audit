200e4af2e79b9980ee26f2517a1cf72a7a6c4cf7
oluwatosinjegede <jegedeoluwatosin10@gmail.com>
2026-03-23 01:48:27 +0100

    Add export fuzzy route

--- audit_app/urls.py
+++ audit_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     # Add here
     path('export/invalid/', export_invalid, name='export_invalid'),
     path('export/duplicates/', export_duplicates, name='export_duplicates'),
+
+    path('export/fuzzy/', export_fuzzy, name='export_fuzzy'),
 ]
